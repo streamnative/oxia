8a9aab2cecea4f8c32fe98e602ddc42ed69a5b56
Dave Maughan <davidamaughan@gmail.com>
2022-11-10 14:36:50 +0000

    Client interface and memory impl & tests (#26)
    * Client interface and memory impl & tests
    
    * put expected and actual in the correct order
    
    * Use pointers

---
+++ common/clock.go
@@ -0,0 +1,18 @@
+package common
+
+import "time"
+
+type Clock interface {
+	NowMillis() uint64
+}
+
+type systemClock struct {
+}
+
+func SystemClock() Clock {
+	return &systemClock{}
+}
+
+func (c *systemClock) NowMillis() uint64 {
+	return uint64(time.Now().UnixMilli())
+}
