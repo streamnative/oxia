79b391c5a9e419b85b036b9b44bc93395f727129
Dave Maughan <dave.maughan@streamnative.io>
2022-12-12 22:47:38 +0000

    Manage clusters (#121)
    * Coordinator Service
    
    * Coordinator Service
    
    * remove cluster
    
    * ShardsCount -> ShardCount
    
    * Manage clusters
    
    Co-authored-by: Elliot West <elliot.west@streamnative.io>

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"go.uber.org/automaxprocs/maxprocs"
 	"os"
 	"oxia/cmd/client"
+	"oxia/cmd/cluster"
 	"oxia/cmd/controller"
 	"oxia/cmd/coordinator"
 	"oxia/cmd/operator"
@@ -38,6 +39,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&common.PprofBindAddress, "profile-bind-address", "127.0.0.1:6060", "Bind address for pprof")
 
 	rootCmd.AddCommand(client.Cmd)
+	rootCmd.AddCommand(cluster.Cmd)
 	rootCmd.AddCommand(controller.Cmd)
 	rootCmd.AddCommand(coordinator.Cmd)
 	rootCmd.AddCommand(operator.Cmd)
