f63edb0ccbf6f466a7908e5723677afe2a0cf6ed
Matteo Merli <mmerli@apache.org>
2022-12-30 16:05:21 -0800

    Shutdown gRPC health server for faster close()

--- server/internal_rpc_server.go
+++ server/internal_rpc_server.go
@@ -29,6 +29,7 @@ type internalRpcServer struct {
 	shardsDirector       ShardsDirector
 	assignmentDispatcher ShardAssignmentsDispatcher
 	grpcServer           container.GrpcServer
+	healthServer         *health.Server
 	log                  zerolog.Logger
 }
 
@@ -36,6 +37,7 @@ func newCoordinationRpcServer(grpcProvider container.GrpcProvider, bindAddress s
 	server := &internalRpcServer{
 		shardsDirector:       shardsDirector,
 		assignmentDispatcher: assignmentDispatcher,
+		healthServer:         health.NewServer(),
 		log: log.With().
 			Str("component", "coordination-rpc-server").
 			Logger(),
@@ -45,7 +47,7 @@ func newCoordinationRpcServer(grpcProvider container.GrpcProvider, bindAddress s
 	server.grpcServer, err = grpcProvider.StartGrpcServer("internal", bindAddress, func(registrar grpc.ServiceRegistrar) {
 		proto.RegisterOxiaControlServer(registrar, server)
 		proto.RegisterOxiaLogReplicationServer(registrar, server)
-		grpc_health_v1.RegisterHealthServer(registrar, health.NewServer())
+		grpc_health_v1.RegisterHealthServer(registrar, server.healthServer)
 	})
 	if err != nil {
 		return nil, err
@@ -55,6 +57,7 @@ func newCoordinationRpcServer(grpcProvider container.GrpcProvider, bindAddress s
 }
 
 func (s *internalRpcServer) Close() error {
+	s.healthServer.Shutdown()
 	return s.grpcServer.Close()
 }
 
