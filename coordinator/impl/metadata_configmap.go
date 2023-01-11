9c54c0e5f7ac060a650739615e59c588a6a9e102
Matteo Merli <mmerli@apache.org>
2023-01-10 21:24:04 -0800

    Added coordinator metrics (#198)

--- coordinator/impl/metadata_configmap.go
+++ coordinator/impl/metadata_configmap.go
@@ -2,32 +2,54 @@ package impl
 
 import (
 	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/otel/metric/unit"
 	"gopkg.in/yaml.v2"
 	coreV1 "k8s.io/api/core/v1"
 	k8sError "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8s "k8s.io/client-go/kubernetes"
+	"oxia/common/metrics"
 	"oxia/coordinator/model"
 	"oxia/kubernetes"
 	"sync"
+	"sync/atomic"
 )
 
 type metadataProviderConfigMap struct {
 	sync.Mutex
 	kubernetes      k8s.Interface
 	namespace, name string
+
+	metadataSize      atomic.Int64
+	getLatencyHisto   metrics.LatencyHistogram
+	storeLatencyHisto metrics.LatencyHistogram
+	metadataSizeGauge metrics.Gauge
 }
 
-func NewMetadataProviderConfigMap(namespace, name string) MetadataProvider {
-	config := kubernetes.NewClientConfig()
-	return &metadataProviderConfigMap{
-		kubernetes: kubernetes.NewKubernetesClientset(config),
+func NewMetadataProviderConfigMap(k8s k8s.Interface, namespace, name string) MetadataProvider {
+	m := &metadataProviderConfigMap{
+		kubernetes: k8s,
 		namespace:  namespace,
 		name:       name,
+
+		getLatencyHisto: metrics.NewLatencyHistogram("oxia_coordinator_metadata_get_latency",
+			"Latency for reading coordinator metadata", nil),
+		storeLatencyHisto: metrics.NewLatencyHistogram("oxia_coordinator_metadata_store_latency",
+			"Latency for storing coordinator metadata", nil),
 	}
+
+	m.metadataSizeGauge = metrics.NewGauge("oxia_coordinator_metadata_size",
+		"The size of the coordinator metadata", unit.Bytes, nil, func() int64 {
+			return m.metadataSize.Load()
+		})
+
+	return m
 }
 
 func (m *metadataProviderConfigMap) Get() (status *model.ClusterStatus, version Version, err error) {
+	timer := m.getLatencyHisto.Timer()
+	defer timer.Done()
+
 	m.Lock()
 	defer m.Unlock()
 	return m.getWithoutLock()
@@ -44,14 +66,19 @@ func (m *metadataProviderConfigMap) getWithoutLock() (status *model.ClusterStatu
 	}
 
 	status = &model.ClusterStatus{}
-	if err = yaml.Unmarshal([]byte(cm.Data["status"]), status); err != nil {
+	data := []byte(cm.Data["status"])
+	if err = yaml.Unmarshal(data, status); err != nil {
 		return
 	}
 	version = Version(cm.ResourceVersion)
+	m.metadataSize.Store(int64(len(data)))
 	return
 }
 
 func (m *metadataProviderConfigMap) Store(status *model.ClusterStatus, expectedVersion Version) (version Version, err error) {
+	timer := m.storeLatencyHisto.Timer()
+	defer timer.Done()
+
 	m.Lock()
 	defer m.Unlock()
 
@@ -65,11 +92,13 @@ func (m *metadataProviderConfigMap) Store(status *model.ClusterStatus, expectedV
 		return
 	}
 
-	cm, err := kubernetes.ConfigMaps(m.kubernetes).Upsert(m.namespace, configMap(m.name, status, expectedVersion))
+	data := configMap(m.name, status, expectedVersion)
+	cm, err := kubernetes.ConfigMaps(m.kubernetes).Upsert(m.namespace, data)
 	if k8sError.IsConflict(err) {
 		err = ErrorMetadataBadVersion
 	}
 	version = Version(cm.ResourceVersion)
+	m.metadataSize.Store(int64(len(data.Data["status"])))
 	return
 }
 
--- coordinator/impl/shard_controller.go
+++ coordinator/impl/shard_controller.go
@@ -12,6 +12,7 @@ import (
 	"io"
 	"math/rand"
 	"oxia/common"
+	"oxia/common/metrics"
 	"oxia/coordinator/model"
 	"oxia/proto"
 	"sync"
@@ -50,9 +51,16 @@ type shardController struct {
 	currentElectionCtx    context.Context
 	currentElectionCancel context.CancelFunc
 	log                   zerolog.Logger
+
+	leaderElectionLatency metrics.LatencyHistogram
+	fenceQuorumLatency    metrics.LatencyHistogram
+	becomeLeaderLatency   metrics.LatencyHistogram
+	leaderElectionsFailed metrics.Counter
+	epochGauge            metrics.Gauge
 }
 
 func NewShardController(shard uint32, shardMetadata model.ShardMetadata, rpc RpcProvider, coordinator Coordinator) ShardController {
+	labels := metrics.LabelsForShard(shard)
 	s := &shardController{
 		shard:         shard,
 		shardMetadata: shardMetadata,
@@ -62,7 +70,21 @@ func NewShardController(shard uint32, shardMetadata model.ShardMetadata, rpc Rpc
 			Str("component", "shard-controller").
 			Uint32("shard", shard).
 			Logger(),
+
+		leaderElectionLatency: metrics.NewLatencyHistogram("oxia_coordinator_leader_election_latency",
+			"The time it takes to elect a leader for the shard", labels),
+		leaderElectionsFailed: metrics.NewCounter("oxia_coordinator_leader_election_failed",
+			"The number of failed leader elections", "count", labels),
+		fenceQuorumLatency: metrics.NewLatencyHistogram("oxia_coordinator_fence_quorum_latency",
+			"The time it takes to fence the ensemble of nodes", labels),
+		becomeLeaderLatency: metrics.NewLatencyHistogram("oxia_coordinator_become_leader_latency",
+			"The time it takes for the new elected leader to start", labels),
 	}
+
+	s.epochGauge = metrics.NewGauge("oxia_coordinator_epoch",
+		"The epoch of the shard", "count", labels, func() int64 {
+			return s.shardMetadata.Epoch
+		})
 
 	s.ctx, s.cancel = context.WithCancel(context.Background())
 
@@ -137,6 +159,7 @@ func (s *shardController) electLeaderWithRetries() {
 	}, func() {
 		_ = backoff.RetryNotify(s.electLeader, common.NewBackOff(s.ctx),
 			func(err error, duration time.Duration) {
+				s.leaderElectionsFailed.Inc()
 				s.log.Warn().Err(err).
 					Dur("retry-after", duration).
 					Msg("Leader election has failed, retrying later")
@@ -145,6 +168,8 @@ func (s *shardController) electLeaderWithRetries() {
 }
 
 func (s *shardController) electLeader() error {
+	timer := s.leaderElectionLatency.Timer()
+
 	s.Lock()
 	defer s.Unlock()
 
@@ -197,6 +222,7 @@ func (s *shardController) electLeader() error {
 		Interface("leader", s.shardMetadata.Leader).
 		Msg("Elected new leader")
 
+	defer timer.Done()
 	s.keepFencingFailedFollowers(followers)
 	return nil
 }
@@ -290,6 +316,8 @@ func (s *shardController) fenceAndAddFollower(ctx context.Context, node model.Se
 // Fence all the ensemble members in parallel and wait for
 // a majority of them to reply successfully
 func (s *shardController) fenceQuorum() (map[model.ServerAddress]*proto.EntryId, error) {
+	timer := s.fenceQuorumLatency.Timer()
+
 	ensembleSize := len(s.shardMetadata.Ensemble)
 	majority := ensembleSize/2 + 1
 
@@ -368,10 +396,12 @@ func (s *shardController) fenceQuorum() (map[model.ServerAddress]*proto.EntryId,
 			}
 
 		case <-time.After(quorumFencingGracePeriod):
+			timer.Done()
 			return res, nil
 		}
 	}
 
+	timer.Done()
 	return res, nil
 }
 
@@ -417,6 +447,8 @@ func (s *shardController) selectNewLeader(fenceResponses map[model.ServerAddress
 }
 
 func (s *shardController) becomeLeader(leader model.ServerAddress, followers map[model.ServerAddress]*proto.EntryId) error {
+	timer := s.leaderElectionLatency.Timer()
+
 	followersMap := make(map[string]*proto.EntryId)
 	for sa, e := range followers {
 		followersMap[sa.Internal] = e
@@ -431,6 +463,7 @@ func (s *shardController) becomeLeader(leader model.ServerAddress, followers map
 		return err
 	}
 
+	timer.Done()
 	return nil
 }
 
@@ -467,5 +500,6 @@ func (s *shardController) Status() model.ShardStatus {
 
 func (s *shardController) Close() error {
 	s.cancel()
+	s.epochGauge.Unregister()
 	return nil
 }
