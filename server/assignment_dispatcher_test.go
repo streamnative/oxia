e98b3d71bbc21fce8a9f4676661994e340ddeba3
Dave Maughan <dave.maughan@streamnative.io>
2023-03-07 14:42:52 +0000

    Single shard generation impl (#273)
    I thought I saw a bug in the coordinator shard generation - there wasn't
    but I was already halfway to standardizing this so I finished it.

--- server/assignment_dispatcher_test.go
+++ server/assignment_dispatcher_test.go
@@ -158,23 +158,3 @@ func newShardAssignment(id uint32, leader string, min uint32, max uint32) *proto
 		},
 	}
 }
-
-func TestShardGenerator(t *testing.T) {
-	assignments := generateShards(4)
-	assertNext(t, assignments[0], 0, 0, 1073741823)
-	assertNext(t, assignments[1], 1, 1073741824, 2147483647)
-	assertNext(t, assignments[2], 2, 2147483648, 3221225471)
-	assertNext(t, assignments[3], 3, 3221225472, math.MaxUint32)
-}
-
-func assertNext(
-	t *testing.T,
-	assignment *proto.ShardAssignment,
-	expectedShardId uint32,
-	expectedLowerBound uint32,
-	expectedUpperBound uint32) {
-	assert.EqualValues(t, expectedShardId, assignment.ShardId)
-	rng := assignment.ShardBoundaries.(*proto.ShardAssignment_Int32HashRange).Int32HashRange
-	assert.EqualValues(t, expectedLowerBound, rng.MinHashInclusive)
-	assert.EqualValues(t, expectedUpperBound, rng.MaxHashInclusive)
-}
