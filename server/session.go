2e068d8a630ae4e0f3e391ea9689e2ef0d287013
Matteo Merli <mmerli@apache.org>
2023-03-08 08:19:12 -0800

    Removed the session on timeout

--- server/session.go
+++ server/session.go
@@ -138,7 +138,6 @@ func (s *session) waitForHeartbeats() {
 
 			s.Lock()
 			s.closeChannels()
-			s.sm.expiredSessions.Inc()
 			err := s.delete()
 
 			if err != nil {
@@ -146,6 +145,11 @@ func (s *session) waitForHeartbeats() {
 					Msg("Failed to delete session")
 			}
 			s.Unlock()
+
+			s.sm.Lock()
+			delete(s.sm.sessions, s.id)
+			s.sm.expiredSessions.Inc()
+			s.sm.Unlock()
 		}
 	}
 }
