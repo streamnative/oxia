44b930a0903e8d691f4f8d1df061c2942e422647
Matteo Merli <mmerli@apache.org>
2023-07-23 09:10:46 +0200

    Added Pebble DB CLI util tool

---
+++ cmd/pebble/cmd.go
@@ -0,0 +1,37 @@
+// Copyright 2023 StreamNative, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pebble
+
+import (
+	"github.com/cockroachdb/pebble/tool"
+	"github.com/spf13/cobra"
+	"oxia/server/kv"
+)
+
+var (
+	Cmd = &cobra.Command{
+		Use:   "pebble",
+		Short: "Pebble DB utils",
+		Long:  `Tools for the Pebble DB`,
+	}
+)
+
+func init() {
+	t := tool.New(tool.DefaultComparer(kv.OxiaSlashSpanComparer))
+
+	for _, cmd := range t.Commands {
+		Cmd.AddCommand(cmd)
+	}
+}
