c53fb3b987a83129a20c7b47b2c60adb643807ca
Mattison Chao <mattisonchao@apache.org>
2024-01-05 10:31:40 +0800

    *: Update license

--- server/wal/wal_test.go
+++ server/wal/wal_test.go
@@ -1,4 +1,4 @@
-// Copyright 2023 StreamNative, Inc.
+// Copyright 2024 StreamNative, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
