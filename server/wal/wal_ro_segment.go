fa7b594288b1ad8ae6fff72a13051d7c971d5087
Qiang Zhao <mattisonchao@apache.org>
2024-10-05 16:25:45 +0800

    fix lint

--- server/wal/wal_ro_segment.go
+++ server/wal/wal_ro_segment.go
@@ -321,7 +321,7 @@ func (r *readOnlySegmentsGroup) PollHighestSegment() (common.RefCount[ReadOnlySe
 	defer r.Unlock()
 
 	if r.allSegments.Empty() {
-		return nil, nil //nolint: nilnil
+		return nil, nil // nolint: nilnil
 	}
 
 	offset, _ := r.allSegments.Max()
