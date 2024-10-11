6778f7fe3e4ffeabfcf3f7257485b68ea9e3e7f1
Qiang Zhao <mattisonchao@apache.org>
2024-10-11 08:08:22 +0800

    feat(wal): support crc for index file (#549)
    ### Motivation
    
    support simple CRC for index file to help detect and auto-recover the
    index file.
    
    ### Modification
    
    - Intorduce v2 idx extension `.idxx`.
    - extract some idx related logic into codec.
    
    ---------
    
    Co-authored-by: Matteo Merli <mmerli@apache.org>

--- server/wal/codec/v1.go
+++ server/wal/codec/v1.go
@@ -16,16 +16,25 @@ package codec
 
 import (
 	"encoding/binary"
-
 	"github.com/pkg/errors"
+	"go.uber.org/multierr"
+	"io"
+	"os"
 )
 
+// Txn File:
 // +--------------+--------------+
-// | Size(4Bytes) | Payload(...) |
+// | Size(4 Bytes) | Payload(...) |
 // +--------------+--------------+
 // Size: 			Length of the payload data
 // Payload: 		Byte stream as long as specified by the payload size.
-var _ Codec = V1{}
+
+// Idx File:
+// +----------------+----------------+----------------+
+// | Index(4 Bytes) | Index(4 Bytes) | 	... 		  |
+// +----------------+----------------+----------------+
+// Index: 			The file offset index
+var _ Codec = &V1{}
 
 const v1PayloadSizeLen uint32 = 4
 const v1TxnExtension = ".txn"
@@ -33,9 +42,10 @@ const v1IdxExtension = ".idx"
 
 var v1 = &V1{
 	Metadata{
-		TxnExtension: v1TxnExtension,
-		IdxExtension: v1IdxExtension,
-		HeaderSize:   v1PayloadSizeLen,
+		TxnExtension:  v1TxnExtension,
+		IdxExtension:  v1IdxExtension,
+		HeaderSize:    v1PayloadSizeLen,
+		IdxHeaderSize: 0,
 	},
 }
 
@@ -43,19 +53,19 @@ type V1 struct {
 	Metadata
 }
 
-func (v V1) GetIdxExtension() string {
+func (v *V1) GetIdxExtension() string {
 	return v.IdxExtension
 }
 
-func (v V1) GetTxnExtension() string {
+func (v *V1) GetTxnExtension() string {
 	return v.TxnExtension
 }
 
-func (v V1) GetHeaderSize() uint32 {
+func (v *V1) GetHeaderSize() uint32 {
 	return v.HeaderSize
 }
 
-func (v V1) ReadRecordWithValidation(buf []byte, startFileOffset uint32) (payload []byte, err error) {
+func (v *V1) ReadRecordWithValidation(buf []byte, startFileOffset uint32) (payload []byte, err error) {
 	var payloadSize uint32
 	if payloadSize, _, _, err = v.ReadHeaderWithValidation(buf, startFileOffset); err != nil {
 		return nil, err
@@ -66,30 +76,28 @@ func (v V1) ReadRecordWithValidation(buf []byte, startFileOffset uint32) (payloa
 	return payload, nil
 }
 
-func (v V1) GetRecordSize(buf []byte, startFileOffset uint32) (payloadSize uint32, err error) {
+func (v *V1) GetRecordSize(buf []byte, startFileOffset uint32) (payloadSize uint32, err error) {
 	if payloadSize, _, _, err = v.ReadHeaderWithValidation(buf, startFileOffset); err != nil {
 		return 0, err
 	}
 	return v.HeaderSize + payloadSize, nil
 }
 
-func (v V1) ReadHeaderWithValidation(buf []byte, startFileOffset uint32) (payloadSize uint32, previousCrc uint32, payloadCrc uint32, err error) {
+func (v *V1) ReadHeaderWithValidation(buf []byte, startFileOffset uint32) (payloadSize uint32, previousCrc uint32, payloadCrc uint32, err error) {
 	bufSize := uint32(len(buf))
 	if startFileOffset >= bufSize {
 		return payloadSize, previousCrc, payloadCrc, errors.Wrapf(ErrOffsetOutOfBounds,
 			"expected payload size: %d. actual buf size: %d ", startFileOffset+v1PayloadSizeLen, bufSize)
 	}
 
-	var headerOffset uint32
 	payloadSize = ReadInt(buf, startFileOffset)
-	headerOffset += v1PayloadSizeLen
 	// It shouldn't happen when normal reading
 	if payloadSize == 0 {
 		return payloadSize, previousCrc, payloadCrc, errors.Wrapf(ErrEmptyPayload, "unexpected empty payload")
 	}
 	expectSize := payloadSize + v.HeaderSize
 	// overflow checking
-	actualBufSize := bufSize - (startFileOffset + headerOffset)
+	actualBufSize := bufSize - startFileOffset
 	if expectSize > actualBufSize {
 		return payloadSize, previousCrc, payloadCrc,
 			errors.Wrapf(ErrOffsetOutOfBounds, "expected payload size: %d. actual buf size: %d ", expectSize, bufSize)
@@ -97,7 +105,7 @@ func (v V1) ReadHeaderWithValidation(buf []byte, startFileOffset uint32) (payloa
 	return payloadSize, previousCrc, payloadCrc, nil
 }
 
-func (V1) WriteRecord(buf []byte, startOffset uint32, _ uint32, payload []byte) (recordSize uint32, payloadCrc uint32) {
+func (*V1) WriteRecord(buf []byte, startOffset uint32, _ uint32, payload []byte) (recordSize uint32, payloadCrc uint32) {
 	payloadSize := uint32(len(payload))
 
 	var headerOffset uint32
@@ -106,4 +114,63 @@ func (V1) WriteRecord(buf []byte, startOffset uint32, _ uint32, payload []byte) 
 
 	copy(buf[startOffset+headerOffset:], payload)
 	return headerOffset + payloadSize, payloadCrc
+}
+
+func (*V1) WriteIndex(path string, index []byte) error {
+	idxFile, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open index file %s", path)
+	}
+
+	if _, err = idxFile.Write(index); err != nil {
+		return errors.Wrapf(err, "failed write index file %s", path)
+	}
+	return idxFile.Close()
+}
+
+func (*V1) ReadIndex(path string) ([]byte, error) {
+	var idFile *os.File
+	var err error
+	if idFile, err = os.OpenFile(path, os.O_RDONLY, 0); err != nil {
+		return nil, errors.Wrapf(err, "failed to open segment index file %s", path)
+	}
+	var indexBuf []byte
+	if indexBuf, err = io.ReadAll(idFile); err != nil {
+		return nil, multierr.Combine(
+			errors.Wrapf(err, "failed to read segment index file %s", path),
+			idFile.Close())
+	}
+	if err = idFile.Close(); err != nil {
+		return nil, errors.Wrapf(err, "failed to close segment index file %s", path)
+	}
+	return indexBuf, nil
+}
+
+func (v *V1) GetIndexHeaderSize() uint32 {
+	return v.IdxHeaderSize
+}
+
+func (v *V1) RecoverIndex(buf []byte, startFileOffset uint32, baseEntryOffset int64,
+	_ *int64) (index []byte, lastCrc uint32, newFileOffset uint32, lastEntryOffset int64, err error) {
+	maxSize := uint32(len(buf))
+	newFileOffset = startFileOffset
+	index = BorrowEmptyIndexBuf()
+	currentEntryOffset := baseEntryOffset
+
+	for newFileOffset < maxSize {
+		var payloadSize uint32
+		var err error
+		if payloadSize, _, _, err = v.ReadHeaderWithValidation(buf, newFileOffset); err != nil {
+			if errors.Is(err, ErrEmptyPayload) || errors.Is(err, ErrOffsetOutOfBounds) {
+				// we might read the end of the segment.
+				break
+			}
+			return nil, 0, 0, 0, err
+		}
+		index = binary.BigEndian.AppendUint32(index, newFileOffset)
+		newFileOffset += v.GetHeaderSize() + payloadSize
+		currentEntryOffset++
+	}
+
+	return index, lastCrc, newFileOffset, currentEntryOffset - 1, nil
 }
--- server/wal/wal_ro_segment.go
+++ server/wal/wal_ro_segment.go
@@ -17,6 +17,7 @@ package wal
 import (
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"sync"
@@ -67,8 +68,7 @@ type readonlySegment struct {
 	txnMappedFile mmap.MMap
 
 	// Index file maps a logical "offset" to a physical file offset within the wal segment
-	idxFile       *os.File
-	idxMappedFile mmap.MMap
+	idx           []byte
 	openTimestamp time.Time
 }
 
@@ -94,18 +94,28 @@ func newReadOnlySegment(basePath string, baseOffset int64) (ReadOnlySegment, err
 		return nil, errors.Wrapf(err, "failed to map segment txn file %s", ms.txnPath)
 	}
 
-	if ms.idxFile, err = os.OpenFile(ms.idxPath, os.O_RDONLY, 0); err != nil {
-		return nil, errors.Wrapf(err, "failed to open segment index file %s", ms.idxPath)
+	if ms.idx, err = ms.codec.ReadIndex(ms.idxPath); err != nil {
+		if !errors.Is(err, codec.ErrDataCorrupted) {
+			return nil, errors.Wrapf(err, "failed to decode segment index file %s", ms.idxPath)
+		}
+		slog.Warn("The segment index file is corrupted and the index is being rebuilt.", slog.String("path", ms.idxPath))
+		// recover from txn
+		if ms.idx, _, _, _, err = ms.codec.RecoverIndex(ms.txnMappedFile, 0,
+			ms.baseOffset, nil); err != nil {
+			slog.Error("The segment index file rebuild failed.", slog.String("path", ms.idxPath))
+			return nil, errors.Wrapf(err, "failed to rebuild segment index file %s", ms.idxPath)
+		}
+		slog.Info("The segment index file has been rebuilt.", slog.String("path", ms.idxPath))
+		if err := ms.codec.WriteIndex(ms.idxPath, ms.idx); err != nil {
+			slog.Warn("write recovered segment index failed. it can continue work but will retry writing after restart.",
+				slog.String("path", ms.idxPath))
+		}
 	}
 
-	if ms.idxMappedFile, err = mmap.MapRegion(ms.idxFile, -1, mmap.RDONLY, 0, 0); err != nil {
-		return nil, errors.Wrapf(err, "failed to map segment index file %s", ms.idxPath)
-	}
-
-	ms.lastOffset = ms.baseOffset + int64(len(ms.idxMappedFile)/4-1)
+	ms.lastOffset = ms.baseOffset + int64(len(ms.idx)/4-1)
 
 	// recover the last crc
-	fo := fileOffset(ms.idxMappedFile, ms.baseOffset, ms.lastOffset)
+	fo := fileOffset(ms.idx, ms.baseOffset, ms.lastOffset)
 	if _, _, ms.lastCrc, err = ms.codec.ReadHeaderWithValidation(ms.txnMappedFile, fo); err != nil {
 		return nil, err
 	}
@@ -128,7 +138,7 @@ func (ms *readonlySegment) Read(offset int64) ([]byte, error) {
 	if offset < ms.baseOffset || offset > ms.lastOffset {
 		return nil, codec.ErrOffsetOutOfBounds
 	}
-	fileReadOffset := fileOffset(ms.idxMappedFile, ms.baseOffset, offset)
+	fileReadOffset := fileOffset(ms.idx, ms.baseOffset, offset)
 	var payload []byte
 	var err error
 	if payload, err = ms.codec.ReadRecordWithValidation(ms.txnMappedFile, fileReadOffset); err != nil {
@@ -149,8 +159,6 @@ func (ms *readonlySegment) Close() error {
 	return multierr.Combine(
 		ms.txnMappedFile.Unmap(),
 		ms.txnFile.Close(),
-		ms.idxMappedFile.Unmap(),
-		ms.idxFile.Close(),
 	)
 }
 
