2849ebde8aa69c53dc60de3235a0599218134810
Qiang Zhao <mattisonchao@apache.org>
2025-02-21 14:26:14 +0800

    step commit

--- coordinator/ensemble/policy_selector.go
+++ coordinator/ensemble/policy_selector.go
@@ -7,8 +7,26 @@ import (
 
 type PolicySelector struct {
 	nodes sync.Map
+
+	antiAffinityPolicy model.AntiAffinityPolicy
 }
 
-func (l *PolicySelector) Select() []model.NodeInfo {
+func (pl *PolicySelector) Select() []model.NodeInfo {
+	antiLabels := pl.antiAffinityPolicy.Labels
 
+	candidates := make([]*model.NodeInfo, 0)
+	pl.nodes.Range(func(key, value any) bool {
+		currentNode := value.(*model.NodeInfo)
+	nodeLoop:
+		for _, candidate := range candidates {
+			for _, antiLabel := range antiLabels {
+				label, ok := candidate.Labels[antiLabel]
+				if !ok {
+					break nodeLoop
+				}
+
+			}
+		}
+		candidates = append(candidates, info)
+	})
 }
