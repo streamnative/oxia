c11420291624eb1f67e75e064e7d4128431bd232
Qiang Zhao <mattisonchao@apache.org>
2025-03-31 18:48:23 +0800

    fix lint

--- coordinator/ensemble/server_idx_selector.go
+++ coordinator/ensemble/server_idx_selector.go
@@ -24,7 +24,7 @@ var _ Selector = &serverIdxSelector{}
 type serverIdxSelector struct {
 }
 
-func (l *serverIdxSelector) SelectNew(
+func (_ *serverIdxSelector) SelectNew(
 	candidates []model.Server,
 	_ map[string]model.ServerMetadata,
 	_ *policies.Policies,
