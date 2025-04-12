7d3f903d9ec9c7c86ee320128dd1e1257099d8a9
Qiang Zhao <mattisonchao@apache.org>
2025-04-12 23:19:04 +0800

    make the test more stable

--- tests/checkpoint/checkpoint_test.go
+++ tests/checkpoint/checkpoint_test.go
@@ -16,6 +16,7 @@ package checkpoint
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -30,129 +31,158 @@ import (
 	"github.com/streamnative/oxia/tests/utils"
 )
 
-func TestCheckpoint_VersionID_Discard(t *testing.T) {
-	checkpointEnabled := true
-	checkpointCommitEvery := int32(5)
-	failureHandling := policies.FailureHandlingDiscard
-	coordinator, serverInfos, servers, cleanupFunc := utils.CreateCluster(t, utils.TestClusterOptions{
-		ServerNum: 3,
-		ClusterConfig: model.ClusterConfig{
-			Namespaces: []model.NamespaceConfig{{
-				Name:              common.DefaultNamespace,
-				ReplicationFactor: 3,
-				InitialShardCount: 1,
-				Policies: &policies.Policies{
-					Checkpoint: &policies.Checkpoint{
-						Enabled:         &checkpointEnabled,
-						CommitEvery:     &checkpointCommitEvery,
-						FailureHandling: &failureHandling,
-					},
+func TestCheckpoint_VersionID(t *testing.T) {
+	for _, test := range []struct {
+		name            string
+		failureHandling int32
+	}{
+		{name: "checkpoint-version-id-failure-handling-warn", failureHandling: policies.FailureHandlingWarn},
+		{name: "checkpoint-version-id-failure-handling-discard", failureHandling: policies.FailureHandlingDiscard},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			checkpointEnabled := true
+			checkpointCommitEvery := int32(5)
+			coordinator, serverInfos, servers, cleanupFunc := utils.CreateCluster(t, utils.TestClusterOptions{
+				ServerNum: 3,
+				ClusterConfig: model.ClusterConfig{
+					Namespaces: []model.NamespaceConfig{{
+						Name:              common.DefaultNamespace,
+						ReplicationFactor: 3,
+						InitialShardCount: 1,
+						Policies: &policies.Policies{
+							Checkpoint: &policies.Checkpoint{
+								Enabled:         &checkpointEnabled,
+								CommitEvery:     &checkpointCommitEvery,
+								FailureHandling: &test.failureHandling,
+							},
+						},
+					}},
 				},
-			}},
-		},
-	})
-	defer cleanupFunc()
+			})
+			defer cleanupFunc()
 
-	client, err := oxia.NewSyncClient(serverInfos[0].Public)
-	assert.NoError(t, err)
-	defer client.Close()
+			client, err := oxia.NewSyncClient(serverInfos[0].Public)
+			assert.NoError(t, err)
+			defer client.Close()
 
-	_, _, err = client.Put(context.Background(), "t1", []byte("t1"))
-	assert.NoError(t, err)
-	_, _, err = client.Put(context.Background(), "t2", []byte("t2"))
-	assert.NoError(t, err)
-	_, _, err = client.Put(context.Background(), "t3", []byte("t3"))
-	assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t1", []byte("t1"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t2", []byte("t2"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t3", []byte("t3"))
+			assert.NoError(t, err)
 
-	lc, leaderInfo := utils.GetClusterLeader(t, coordinator, servers, common.DefaultNamespace, 0)
-	lcDB := lc.GetDB()
-	lcCommitOffset, err := lcDB.ReadCommitOffset()
-	assert.NoError(t, err)
-	lcLastVersion, err := lcDB.ReadLastVersionId()
-	assert.NoError(t, err)
+			lc, leaderInfo := utils.GetClusterLeader(t, coordinator, servers, common.DefaultNamespace, 0)
+			lcDB := lc.GetDB()
+			lcCommitOffset, err := lcDB.ReadCommitOffset()
+			assert.NoError(t, err)
+			lcLastVersion, err := lcDB.ReadLastVersionId()
+			assert.NoError(t, err)
 
-	for svID, sv := range servers {
-		if svID == leaderInfo.GetIdentifier() {
-			continue
-		}
-		follower := sv
-		fd := follower.GetShardsDirector()
-		f, err := fd.GetFollower(0)
-		assert.NoError(t, err)
-		db := f.GetDB()
-		assert.Eventually(t, func() bool {
-			fcLastVersion, err := db.ReadLastVersionId()
-			if err != nil {
-				return false
+			dirtyCounter := 2
+			for svID, sv := range servers {
+				if svID == leaderInfo.GetIdentifier() {
+					continue
+				}
+				follower := sv
+				fd := follower.GetShardsDirector()
+				f, err := fd.GetFollower(0)
+				assert.NoError(t, err)
+				db := f.GetDB()
+				assert.Eventually(t, func() bool {
+					fcLastVersion, err := db.ReadLastVersionId()
+					if err != nil {
+						return false
+					}
+					return lcLastVersion-1 == fcLastVersion
+				}, time.Second, time.Millisecond*100)
+
+				// make the follower become dirty
+				puts := make([]*proto.PutRequest, 0)
+				for i := range dirtyCounter {
+					puts = append(puts, &proto.PutRequest{
+						Key:   fmt.Sprintf("dirty-%v", i),
+						Value: []byte("dirty"),
+					})
+				}
+				dirtyCounter += 3
+				_, err = db.ProcessWrite(&proto.WriteRequest{
+					Puts: puts,
+				}, lcCommitOffset, 0, kv.NoOpCallback)
+				assert.NoError(t, err)
+
+				// last version has already dirty
+				fcLastVersion, err := db.ReadLastVersionId()
+				assert.NoError(t, err)
+				assert.NotEqual(t, lcLastVersion-1, fcLastVersion)
 			}
-			return lcLastVersion-1 == fcLastVersion
-		}, time.Second, time.Millisecond*100)
 
-		// make the follower become dirty
-		_, err = db.ProcessWrite(&proto.WriteRequest{
-			Puts: []*proto.PutRequest{
-				{
-					Key:   "dirty",
-					Value: []byte("dirty"),
-				},
-			},
-		}, lcCommitOffset, 0, kv.NoOpCallback)
-		assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t4", []byte("t4"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t5", []byte("t5"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t6", []byte("t6"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t7", []byte("t7"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t8", []byte("t8"))
+			assert.NoError(t, err)
 
-		// last version has already dirty
-		fcLastVersion, err := db.ReadLastVersionId()
-		assert.NoError(t, err)
-		assert.NotEqual(t, lcLastVersion-1, fcLastVersion)
-	}
+			// trigger election
+			coordinator.NodeBecameUnavailable(*leaderInfo)
 
-	_, _, err = client.Put(context.Background(), "t4", []byte("t4"))
-	assert.NoError(t, err)
-	_, _, err = client.Put(context.Background(), "t5", []byte("t5"))
-	assert.NoError(t, err)
-	_, _, err = client.Put(context.Background(), "t6", []byte("t6"))
-	assert.NoError(t, err)
-	_, _, err = client.Put(context.Background(), "t7", []byte("t7"))
-	assert.NoError(t, err)
-	_, _, err = client.Put(context.Background(), "t8", []byte("t8"))
-	assert.NoError(t, err)
+			time.Sleep(2 * time.Second)
+			assert.Eventually(t, func() bool {
+				status := coordinator.ClusterStatus()
+				namespaceStatus := status.Namespaces[common.DefaultNamespace]
+				firstShare := namespaceStatus.Shards[0]
+				return firstShare.Status == model.ShardStatusSteadyState
+			}, time.Second, time.Millisecond*100)
 
-	// trigger election
-	coordinator.NodeBecameUnavailable(*leaderInfo)
+			_, _, err = client.Put(context.Background(), "t9", []byte("t9"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t10", []byte("t10"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t11", []byte("t11"))
+			assert.NoError(t, err)
+			_, _, err = client.Put(context.Background(), "t12", []byte("t12"))
+			assert.NoError(t, err)
 
-	time.Sleep(2 * time.Second)
-	assert.Eventually(t, func() bool {
-		status := coordinator.ClusterStatus()
-		namespaceStatus := status.Namespaces[common.DefaultNamespace]
-		firstShare := namespaceStatus.Shards[0]
-		return firstShare.Status == model.ShardStatusSteadyState
-	}, time.Second, time.Millisecond*100)
+			lc, leaderInfo = utils.GetClusterLeader(t, coordinator, servers, common.DefaultNamespace, 0)
+			lcDB = lc.GetDB()
+			lcLastVersion, err = lcDB.ReadLastVersionId()
+			assert.NoError(t, err)
 
-	_, _, err = client.Put(context.Background(), "t9", []byte("t9"))
-	assert.NoError(t, err)
-
-	_, _, err = client.Put(context.Background(), "t10", []byte("t10"))
-	assert.NoError(t, err)
-
-	lc, leaderInfo = utils.GetClusterLeader(t, coordinator, servers, common.DefaultNamespace, 0)
-	lcDB = lc.GetDB()
-	lcLastVersion, err = lcDB.ReadLastVersionId()
-	assert.NoError(t, err)
-
-	for svID, sv := range servers {
-		if svID == leaderInfo.GetIdentifier() {
-			continue
-		}
-		follower := sv
-		fd := follower.GetShardsDirector()
-		f, err := fd.GetFollower(0)
-		assert.NoError(t, err)
-		db := f.GetDB()
-		assert.Eventually(t, func() bool {
-			fcLastVersion, err := db.ReadLastVersionId()
-			if err != nil {
-				return false
+			for svID, sv := range servers {
+				if svID == leaderInfo.GetIdentifier() {
+					continue
+				}
+				follower := sv
+				fd := follower.GetShardsDirector()
+				f, err := fd.GetFollower(0)
+				assert.NoError(t, err)
+				db := f.GetDB()
+				switch test.failureHandling {
+				case policies.FailureHandlingWarn:
+					assert.Never(t, func() bool {
+						fcLastVersion, err := db.ReadLastVersionId()
+						if err != nil {
+							return false
+						}
+						return lcLastVersion-1 == fcLastVersion
+					}, time.Second, time.Millisecond*100)
+					break
+				case policies.FailureHandlingDiscard:
+					assert.Eventually(t, func() bool {
+						fcLastVersion, err := db.ReadLastVersionId()
+						if err != nil {
+							return false
+						}
+						return lcLastVersion-1 == fcLastVersion
+					}, time.Second, time.Millisecond*100)
+					break
+				}
 			}
-			return lcLastVersion-1 == fcLastVersion
-		}, time.Second, time.Millisecond*100)
+		})
 	}
 }
