79d96e9c9eff88e120e192bb999d1e6744af7939
Qiang Zhao <mattisonchao@apache.org>
2025-05-07 08:15:51 +0800

    fix lint

--- cmd/client/cmd.go
+++ cmd/client/cmd.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
 	"github.com/streamnative/oxia/cmd/client/proofs"
 
 	"github.com/streamnative/oxia/cmd/client/common"
--- cmd/client/proofs/sequence/cmd.go
+++ cmd/client/proofs/sequence/cmd.go
@@ -1,7 +1,22 @@
+// Copyright 2025 StreamNative, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package sequence
 
 import (
 	"github.com/spf13/cobra"
+
 	"github.com/streamnative/oxia/cmd/client/common"
 	"github.com/streamnative/oxia/oxia"
 	"github.com/streamnative/oxia/proofs/workers"
@@ -24,9 +39,5 @@ func exec(cmd *cobra.Command, _ []string) error {
 	if err != nil {
 		return err
 	}
-	proof := workers.NewSequenceProof(client)
-	if err = proof.Bootstrap(cmd.Context()); err != nil {
-		return err
-	}
-	return nil
+	return workers.NewSequenceProof(client).Bootstrap(cmd.Context())
 }
