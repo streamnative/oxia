1ce3fef1787ac8133d8fc8b54e8124df3efcfe35
Matteo Merli <mmerli@apache.org>
2025-05-14 14:43:44 -0700

    In CLI tool range scan, session-id was always printed as 0

--- cmd/client/rangescan/cmd.go
+++ cmd/client/rangescan/cmd.go
@@ -108,6 +108,7 @@ func exec(cmd *cobra.Command, _ []string) error {
 				ModifiedTimestamp:  time.UnixMilli(int64(version.ModifiedTimestamp)),
 				ModificationsCount: version.ModificationsCount,
 				Ephemeral:          version.Ephemeral,
+				SessionId:          version.SessionId,
 				ClientIdentity:     version.ClientIdentity,
 			})
 		}
