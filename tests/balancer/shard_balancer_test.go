f6ee25d95e2a2d6e81ded8cf3570b355229e2486
Qiang Zhao <mattisonchao@apache.org>
2025-06-19 01:08:51 +0800

    remove debugging init

--- tests/balancer/shard_balancer_test.go
+++ tests/balancer/shard_balancer_test.go
@@ -21,18 +21,10 @@ import (
 	"github.com/emirpasic/gods/v2/sets/linkedhashset"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/oxia-db/oxia/common/process"
-
 	"github.com/oxia-db/oxia/coordinator/model"
 	"github.com/oxia-db/oxia/coordinator/policies"
 	"github.com/oxia-db/oxia/tests/mock"
 )
-
-func init() {
-	process.PprofEnable = true
-	process.PprofBindAddress = "127.0.0.1:6060"
-	process.RunProfiling()
-}
 
 func TestNormalShardBalancer(t *testing.T) {
 	s1, s1ad := mock.NewServer(t, "sv-1")
